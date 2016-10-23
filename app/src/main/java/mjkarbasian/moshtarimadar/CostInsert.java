ee8aa62821b4d370b4ba21357e83959450f76c42
javadkarbasian <javad.karbasian@gmail.com>
2016-10-23 21:10:51 +0330

    #create a method of costDataInsertion,Must design sale and detail sale insertion architecture

--- app/src/main/java/mjkarbasian/moshtarimadar/CostInsert.java
+++ app/src/main/java/mjkarbasian/moshtarimadar/CostInsert.java
@@ -1,5 +1,7 @@
 package mjkarbasian.moshtarimadar;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -11,17 +13,24 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import mjkarbasian.moshtarimadar.Data.KasebContract;
 
 /**
  * Created by family on 10/19/2016.
  */
 public class CostInsert extends Fragment {
+    private static final String LOG_TAG = CostInsert.class.getSimpleName() ;
     EditText costName;
     EditText costCode;
     EditText costAmount;
     Spinner costType;
     EditText costDate;
     EditText costDescription;
+    ContentValues costValues = new ContentValues();
+    private Uri insertUri;
+
     public CostInsert() {
         setHasOptionsMenu(true);
 
@@ -35,6 +44,11 @@ public class CostInsert extends Fragment {
          costType = (Spinner) rootView.findViewById(R.id.input_cost_type_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.cost_types, android.R.layout.simple_spinner_item);
+        costName = (EditText)rootView.findViewById(R.id.input_cost_name);
+        costCode = (EditText)rootView.findViewById(R.id.input_cost_code);
+        costAmount = (EditText)rootView.findViewById(R.id.input_cost_amount);
+        costDate = (EditText)rootView.findViewById(R.id.input_cost_date);
+        costDescription = (EditText)rootView.findViewById(R.id.input_cost_description);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         costType.setAdapter(adapter);
 
@@ -50,13 +64,45 @@ public class CostInsert extends Fragment {
     }
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.save:{
+            case R.id.save_inputs:{
+                costValues.put(KasebContract.Costs.COLUMN_COST_NAME,costName.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_COST_CODE,costCode.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_AMOUNT,costAmount.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_DATE,costDate.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_DESCRIPTION,costDescription.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_COST_TYPE_ID,costType.getSelectedItemPosition());
+               insertUri =getActivity().getContentResolver().insert(
+                       KasebContract.Costs.CONTENT_URI,
+                        costValues
+                );
+                //region disabling edit
+                costName.setEnabled(false);
+                costCode.setEnabled(false);
+                costAmount.setEnabled(false);
+                costDescription.setEnabled(false);
+                costType.setEnabled(false);
+                costDate.setEnabled(false);
+                //just a message to show everything are under control
+                Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.msg_insert_succeed),
+                        Toast.LENGTH_LONG).show();
+                
+                checkForValidity();
+                backToLastPage();
 
-               break;
+                break;
             }
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // this method check the validation and correct entries. its check fill first and then check the validation rules.
+    private void checkForValidity() {
+    }
+
+    // this method back to the activity view. this must be a utility method.
+    private void backToLastPage() {
+        getFragmentManager().popBackStackImmediate();
     }
 }
 
