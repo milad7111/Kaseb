b08641e97efdae4c579732098493d7251f3ded13
javadkarbasian <javad.karbasian@gmail.com>
2016-10-29 06:04:20 +0330

    clear preinserted cost data,most add to other insert fragment

--- app/src/main/java/mjkarbasian/moshtarimadar/CostInsert.java
+++ app/src/main/java/mjkarbasian/moshtarimadar/CostInsert.java
@@ -21,7 +21,7 @@ import mjkarbasian.moshtarimadar.Data.KasebContract;
  * Created by family on 10/19/2016.
  */
 public class CostInsert extends Fragment {
-    private static final String LOG_TAG = CostInsert.class.getSimpleName() ;
+    private static final String LOG_TAG = CostInsert.class.getSimpleName();
     EditText costName;
     EditText costCode;
     EditText costAmount;
@@ -38,21 +38,29 @@ public class CostInsert extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
+                             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_cost_insert, container, false);
-
-         costType = (Spinner) rootView.findViewById(R.id.input_cost_type_spinner);
+        costType = (Spinner) rootView.findViewById(R.id.input_cost_type_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
                 R.array.cost_types, android.R.layout.simple_spinner_item);
-        costName = (EditText)rootView.findViewById(R.id.input_cost_name);
-        costCode = (EditText)rootView.findViewById(R.id.input_cost_code);
-        costAmount = (EditText)rootView.findViewById(R.id.input_cost_amount);
-        costDate = (EditText)rootView.findViewById(R.id.input_cost_date);
-        costDescription = (EditText)rootView.findViewById(R.id.input_cost_description);
+        costName = (EditText) rootView.findViewById(R.id.input_cost_name);
+        costCode = (EditText) rootView.findViewById(R.id.input_cost_code);
+        costAmount = (EditText) rootView.findViewById(R.id.input_cost_amount);
+        costDate = (EditText) rootView.findViewById(R.id.input_cost_date);
+        costDescription = (EditText) rootView.findViewById(R.id.input_cost_description);
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         costType.setAdapter(adapter);
 
         return rootView;
+    }
+
+    private void clearEditText() {
+        costName.getText().clear();
+        costCode.getText().clear();
+        costDate.getText().clear();
+        costDescription.getText().clear();
+        costAmount.getText().clear();
     }
 
     @Override
@@ -62,30 +70,24 @@ public class CostInsert extends Fragment {
         menu.removeItem(R.id.search_button);
         inflater.inflate(R.menu.fragments_for_insert, menu);
     }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.save_inputs:{
-                costValues.put(KasebContract.Costs.COLUMN_COST_NAME,costName.getText().toString());
-                costValues.put(KasebContract.Costs.COLUMN_COST_CODE,costCode.getText().toString());
-                costValues.put(KasebContract.Costs.COLUMN_AMOUNT,costAmount.getText().toString());
-                costValues.put(KasebContract.Costs.COLUMN_DATE,costDate.getText().toString());
-                costValues.put(KasebContract.Costs.COLUMN_DESCRIPTION,costDescription.getText().toString());
-                costValues.put(KasebContract.Costs.COLUMN_COST_TYPE_ID,costType.getSelectedItemPosition());
-               insertUri =getActivity().getContentResolver().insert(
-                       KasebContract.Costs.CONTENT_URI,
+            case R.id.save_inputs: {
+                costValues.put(KasebContract.Costs.COLUMN_COST_NAME, costName.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_COST_CODE, costCode.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_AMOUNT, costAmount.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_DATE, costDate.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_DESCRIPTION, costDescription.getText().toString());
+                costValues.put(KasebContract.Costs.COLUMN_COST_TYPE_ID, costType.getSelectedItemPosition());
+                insertUri = getActivity().getContentResolver().insert(
+                        KasebContract.Costs.CONTENT_URI,
                         costValues
                 );
-                //region disabling edit
-                costName.setEnabled(false);
-                costCode.setEnabled(false);
-                costAmount.setEnabled(false);
-                costDescription.setEnabled(false);
-                costType.setEnabled(false);
-                costDate.setEnabled(false);
                 //just a message to show everything are under control
                 Toast.makeText(getActivity(), getActivity().getResources().getString(R.string.msg_insert_succeed),
                         Toast.LENGTH_LONG).show();
-                
+
                 checkForValidity();
                 backToLastPage();
 
@@ -102,6 +104,7 @@ public class CostInsert extends Fragment {
 
     // this method back to the activity view. this must be a utility method.
     private void backToLastPage() {
+        clearEditText();
         getFragmentManager().popBackStackImmediate();
     }
 }
